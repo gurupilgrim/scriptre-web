e6aaafcc011504647bae0429cf32d012e96e2b7a
Nate Moss <nate@gurupilgrim.com>
2019-10-31 04:45:58 -0400

    refactor js

--- js/scriptre.js
+++ js/scriptre.js
@@ -3,6 +3,7 @@ var server = "localhost";
 var leadBook = "";
 var leadChapter = "";
 var leadVerse = "";
+var responseData;
 
 document.getElementById("searchBox").onkeydown = function(event) {
 	if (event.keyCode == 13) {
@@ -10,9 +11,45 @@ document.getElementById("searchBox").onkeydown = function(event) {
 	};
 };
 
-
 function loadVerses() {
-  var queryText = document.getElementById("searchBox").value
+  
+  var queryText = document.getElementById("searchBox").value;
+  getVerses(queryText);
+
+};
+
+function clearContent() {
+  var verseDivs = document.getElementsByClassName("verse_box");
+  if (verseDivs) {
+    while(verseDivs[0]) {
+      verseDivs[0].parentNode.removeChild(verseDivs[0]);
+    };
+  };
+};
+
+function getVerses(query) {
+  var ajaxRequest = new XMLHttpRequest();
+  var apiRequest = "http://" + server + "/v0/query?query=" + query
+  ajaxRequest.open("GET", apiRequest, true);
+
+  ajaxRequest.onload = function() {
+	responseData = JSON.parse(ajaxRequest.responseText);
+	clearContent()
+	makeVerseBox(responseData);
+  	document.getElementById("simpleInfo").innerHTML = responseData['Reference']['Book'];
+	leadBook = responseData['Reference']['Book']
+	leadChapter = responseData['Reference']['Chapter']
+	leadVerse = responseData['Reference']['VerseNumber']
+  };
+  ajaxRequest.onerror = function() {
+  	alert("Error!");
+  };
+  //ajaxRequest.responseType = 'json';
+  ajaxRequest.send();
+  console.log(ajaxRequest);
+};
+
+function makeVerseBox(verseData) {
   var verseDiv = document.createElement("div");
   verseDiv.className = "verse_box";
   var verseNumDiv = document.createElement("div");
@@ -23,32 +60,35 @@ function loadVerses() {
   var verseNumP = document.createElement("p");
   var verseTextP = document.createElement("p");
 
+  verseNumP.innerHTML = verseData['Reference']['Chapter'] + ":" + verseData['Reference']['VerseNumber'];
+  verseTextP.innerHTML = verseData['Text'];
+
+  verseNumDiv.appendChild(verseNumP);
+  verseTextDiv.appendChild(verseTextP);
+  verseDiv.appendChild(verseNumDiv);
+  verseDiv.appendChild(verseTextDiv);
+  document.getElementById("content").appendChild(verseDiv);
+};
+
+
+function previous(mode) {
   var ajaxRequest = new XMLHttpRequest();
-  var apiRequest = "http://" + server + "/v0/query?query=" + queryText
+  if (!mode) {
+	mode = "verse"
+	var apiRequest = "http://" + server + "/v0/previous?ref=" + leadBook + "~" + leadChapter + "." + leadVerse;
+  } else if (mode == "chapter") {
+	var apiRequest = "http://" + server + "/v0/previous?ref=" + leadBook + "~" + leadChapter + "." + leadVerse + "&mode=chapter";
+  };
   ajaxRequest.open("GET", apiRequest, true);
 
   ajaxRequest.onload = function() {
-	var responseData = JSON.parse(ajaxRequest.responseText);
+	responseData = JSON.parse(ajaxRequest.responseText);
+	clearContent()
+	makeVerseBox(responseData);
+  	document.getElementById("simpleInfo").innerHTML = responseData['Reference']['Book'];
 	leadBook = responseData['Reference']['Book']
 	leadChapter = responseData['Reference']['Chapter']
 	leadVerse = responseData['Reference']['VerseNumber']
-
-	verseNumP.innerHTML = responseData['Reference']['Chapter'] + ":" + responseData['Reference']['VerseNumber'];
-	verseTextP.innerHTML = responseData['Text'];
-
-	verseNumDiv.appendChild(verseNumP);
-	verseTextDiv.appendChild(verseTextP);
-  	verseDiv.appendChild(verseNumDiv);
-  	verseDiv.appendChild(verseTextDiv);
-	var verseDivs = document.getElementsByClassName("verse_box");
-	if (verseDivs) {
-	  while(verseDivs[0]) {
-		  verseDivs[0].parentNode.removeChild(verseDivs[0]);
-	  }
-	}
-	document.getElementById("content").appendChild(verseDiv);
-	
-	document.getElementById("simpleInfo").innerHTML = responseData['Reference']['Book']
   };
   ajaxRequest.onerror = function() {
   	alert("Error!");
@@ -58,43 +98,24 @@ function loadVerses() {
   console.log(ajaxRequest);
 };
 
-function previousVerse() {
-  var verseDiv = document.createElement("div");
-  verseDiv.className = "verse_box";
-  var verseNumDiv = document.createElement("div");
-  verseNumDiv.className = "verse_number";
-  var verseTextDiv = document.createElement("div");
-  verseTextDiv.className = "verse_text";
-
-  var verseNumP = document.createElement("p");
-  var verseTextP = document.createElement("p");
-
+function next(mode) {
   var ajaxRequest = new XMLHttpRequest();
-  var apiRequest = "http://" + server + "/v0/previous?ref=" + leadBook + "~" + leadChapter + "." + leadVerse;
+  if (!mode) {
+	mode = "verse"
+	var apiRequest = "http://" + server + "/v0/next?ref=" + leadBook + "~" + leadChapter + "." + leadVerse;
+  } else if (mode == "chapter") {
+	var apiRequest = "http://" + server + "/v0/next?ref=" + leadBook + "~" + leadChapter + "." + leadVerse + "&mode=chapter";
+  };
   ajaxRequest.open("GET", apiRequest, true);
 
   ajaxRequest.onload = function() {
-	var responseData = JSON.parse(ajaxRequest.responseText);
+	responseData = JSON.parse(ajaxRequest.responseText);
+	clearContent()
+	makeVerseBox(responseData);
+  	document.getElementById("simpleInfo").innerHTML = responseData['Reference']['Book'];
 	leadBook = responseData['Reference']['Book']
 	leadChapter = responseData['Reference']['Chapter']
 	leadVerse = responseData['Reference']['VerseNumber']
-
-	verseNumP.innerHTML = responseData['Reference']['Chapter'] + ":" + responseData['Reference']['VerseNumber'];
-	verseTextP.innerHTML = responseData['Text'];
-
-	verseNumDiv.appendChild(verseNumP);
-	verseTextDiv.appendChild(verseTextP);
-  	verseDiv.appendChild(verseNumDiv);
-  	verseDiv.appendChild(verseTextDiv);
-	var verseDivs = document.getElementsByClassName("verse_box");
-	if (verseDivs) {
-	  while(verseDivs[0]) {
-		  verseDivs[0].parentNode.removeChild(verseDivs[0]);
-	  }
-	}
-	document.getElementById("content").appendChild(verseDiv);
-	
-	document.getElementById("simpleInfo").innerHTML = responseData['Reference']['Book']
   };
   ajaxRequest.onerror = function() {
   	alert("Error!");
@@ -102,146 +123,5 @@ function previousVerse() {
   //ajaxRequest.responseType = 'json';
   ajaxRequest.send();
   console.log(ajaxRequest);
-	
 };
 
-function previousChapter() {
-  var verseDiv = document.createElement("div");
-  verseDiv.className = "verse_box";
-  var verseNumDiv = document.createElement("div");
-  verseNumDiv.className = "verse_number";
-  var verseTextDiv = document.createElement("div");
-  verseTextDiv.className = "verse_text";
-
-  var verseNumP = document.createElement("p");
-  var verseTextP = document.createElement("p");
-
-  var ajaxRequest = new XMLHttpRequest();
-  var apiRequest = "http://" + server + "/v0/previous?mode=chapter&ref=" + leadBook + "~" + leadChapter + "." + leadVerse;
-  ajaxRequest.open("GET", apiRequest, true);
-
-  ajaxRequest.onload = function() {
-	var responseData = JSON.parse(ajaxRequest.responseText);
-	leadBook = responseData['Reference']['Book']
-	leadChapter = responseData['Reference']['Chapter']
-	leadVerse = responseData['Reference']['VerseNumber']
-
-	verseNumP.innerHTML = responseData['Reference']['Chapter'] + ":" + responseData['Reference']['VerseNumber'];
-	verseTextP.innerHTML = responseData['Text'];
-
-	verseNumDiv.appendChild(verseNumP);
-	verseTextDiv.appendChild(verseTextP);
-  	verseDiv.appendChild(verseNumDiv);
-  	verseDiv.appendChild(verseTextDiv);
-	var verseDivs = document.getElementsByClassName("verse_box");
-	if (verseDivs) {
-	  while(verseDivs[0]) {
-		  verseDivs[0].parentNode.removeChild(verseDivs[0]);
-	  }
-	}
-	document.getElementById("content").appendChild(verseDiv);
-	
-	document.getElementById("simpleInfo").innerHTML = responseData['Reference']['Book']
-  };
-  ajaxRequest.onerror = function() {
-  	alert("Error!");
-  };
-  //ajaxRequest.responseType = 'json';
-  ajaxRequest.send();
-  console.log(ajaxRequest);
-	
-};
-
-function nextVerse() {
-  var verseDiv = document.createElement("div");
-  verseDiv.className = "verse_box";
-  var verseNumDiv = document.createElement("div");
-  verseNumDiv.className = "verse_number";
-  var verseTextDiv = document.createElement("div");
-  verseTextDiv.className = "verse_text";
-
-  var verseNumP = document.createElement("p");
-  var verseTextP = document.createElement("p");
-
-  var ajaxRequest = new XMLHttpRequest();
-  var apiRequest = "http://" + server + "/v0/next?ref=" + leadBook + "~" + leadChapter + "." + leadVerse;
-  ajaxRequest.open("GET", apiRequest, true);
-
-  ajaxRequest.onload = function() {
-	var responseData = JSON.parse(ajaxRequest.responseText);
-	leadBook = responseData['Reference']['Book']
-	leadChapter = responseData['Reference']['Chapter']
-	leadVerse = responseData['Reference']['VerseNumber']
-
-	verseNumP.innerHTML = responseData['Reference']['Chapter'] + ":" + responseData['Reference']['VerseNumber'];
-	verseTextP.innerHTML = responseData['Text'];
-
-	verseNumDiv.appendChild(verseNumP);
-	verseTextDiv.appendChild(verseTextP);
-  	verseDiv.appendChild(verseNumDiv);
-  	verseDiv.appendChild(verseTextDiv);
-	var verseDivs = document.getElementsByClassName("verse_box");
-	if (verseDivs) {
-	  while(verseDivs[0]) {
-		  verseDivs[0].parentNode.removeChild(verseDivs[0]);
-	  }
-	}
-	document.getElementById("content").appendChild(verseDiv);
-	
-	document.getElementById("simpleInfo").innerHTML = responseData['Reference']['Book']
-  };
-  ajaxRequest.onerror = function() {
-  	alert("Error!");
-  };
-  //ajaxRequest.responseType = 'json';
-  ajaxRequest.send();
-  console.log(ajaxRequest);
-	
-};
-
-function nextChapter() {
-  var verseDiv = document.createElement("div");
-  verseDiv.className = "verse_box";
-  var verseNumDiv = document.createElement("div");
-  verseNumDiv.className = "verse_number";
-  var verseTextDiv = document.createElement("div");
-  verseTextDiv.className = "verse_text";
-
-  var verseNumP = document.createElement("p");
-  var verseTextP = document.createElement("p");
-
-  var ajaxRequest = new XMLHttpRequest();
-  var apiRequest = "http://" + server + "/v0/next?mode=chapter&ref=" + leadBook + "~" + leadChapter + "." + leadVerse;
-  ajaxRequest.open("GET", apiRequest, true);
-
-  ajaxRequest.onload = function() {
-	var responseData = JSON.parse(ajaxRequest.responseText);
-	leadBook = responseData['Reference']['Book']
-	leadChapter = responseData['Reference']['Chapter']
-	leadVerse = responseData['Reference']['VerseNumber']
-
-	verseNumP.innerHTML = responseData['Reference']['Chapter'] + ":" + responseData['Reference']['VerseNumber'];
-	verseTextP.innerHTML = responseData['Text'];
-
-	verseNumDiv.appendChild(verseNumP);
-	verseTextDiv.appendChild(verseTextP);
-  	verseDiv.appendChild(verseNumDiv);
-  	verseDiv.appendChild(verseTextDiv);
-	var verseDivs = document.getElementsByClassName("verse_box");
-	if (verseDivs) {
-	  while(verseDivs[0]) {
-		  verseDivs[0].parentNode.removeChild(verseDivs[0]);
-	  }
-	}
-	document.getElementById("content").appendChild(verseDiv);
-	
-	document.getElementById("simpleInfo").innerHTML = responseData['Reference']['Book']
-  };
-  ajaxRequest.onerror = function() {
-  	alert("Error!");
-  };
-  //ajaxRequest.responseType = 'json';
-  ajaxRequest.send();
-  console.log(ajaxRequest);
-	
-};
